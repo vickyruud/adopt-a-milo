7a8b4404aa6d0e7e301cbcf9b9d60c5abc009b37
vickyruud <aprajit.k@gmail.com>
2022-05-10 14:58:25 -0400

    add basic styles and start navBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import NavBar from './components/NavBar'
+import './styles/app.css'
 
 function App() {
   return (
-    <div>Adopt A Milo</div>
+    <div>
+      <NavBar />
+      <div className="container">
+        <article>
+          <h1>Adopt a Milo </h1>
+          Adopting a pet is Cool Beans!!
+        </article>
+      </div>
+    </div>
   )
 }
 
