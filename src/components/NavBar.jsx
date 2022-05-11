b66a04869fc6afe3c5cefc0d8b9b559e89b35872
vickyruud <aprajit.k@gmail.com>
2022-05-11 15:01:46 -0400

    add toggle component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,7 +25,6 @@ function NavBar() {
       </a>
 
         <Favorite className="md:hidden flex" count={8} />
-        <ToggleSwitch/>
         {showNav ? <HiMenuAlt3
           onClick={() => setShowNav(!showNav)}
           className={(dark ? 'text-white' : null) + ' md:hidden block w-10 h-10 p-2 cursor-pointer' } />  :<HiMenuAlt2
@@ -33,6 +32,7 @@ function NavBar() {
           className={(dark ? 'text-white' : null) + ' md:hidden block w-10 h-10 p-2 cursor-pointer' }/>}
       </div>
       <ul className={(showNav ? "left-0" : "-left-full") + " md:static fixed  bottom-0 top-12 md:flex md:space-x-7 items-center md:bg-transparent md:text-gray-500 bg-gray-900 bg-opacity-90 md:w-auto w-10/12 text-white md:space-y-0 space-y-5 p-2 transition-left"}>
+        
         <NavItem content="Home" href="/home" />
         <NavItem content="Sign Up" href="/signup" />
         <NavItem content="Login" href="/signin" />
--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,13 +1,15 @@
-
 import React from "react";
+import { HiMoon, HiSun } from "react-icons/hi";
   
 const ToggleSwitch = () => {
   return (
-  <label className="flex items-center cursor-pointer relative mb-4" htmlFor="toggle-switch">
-  <input type="checkbox" id="toggle-switch" className="sr-only"/>
-  <div className="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full"></div>
-  <span className="ml-3 text-gray-900 text-sm font-medium">Toggle me</span>
-</label>
+  <>      
+      <label className="flex items-center cursor-pointer relative mb-4" htmlFor="toggle-switch">
+      <input type="checkbox" id="toggle-switch" className="sr-only"/>
+      <div className="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full"></div>
+      <span className="ml-3 text-gray-900 text-sm font-medium"><HiMoon/></span>
+    </label>
+  </>
   );
 };
   
