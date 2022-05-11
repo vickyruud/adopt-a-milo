eb7e388c852ff85d50193520cec6d1d898d5c38f
vickyruud <aprajit.k@gmail.com>
2022-05-11 00:51:27 -0400

    make navbar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../styles/navbar.css'
 import Favorite from './Favorite';
 import NavItem from './NavItem';
-import { AiOutlineMenu } from "react-icons/ai";
+import { HiMenuAlt3, HiMenuAlt2 } from "react-icons/hi";
 
 function NavBar() {
 
@@ -14,15 +14,23 @@ function NavBar() {
       <a href='/'>
         <img className='w-12 h-12 p-2' src='../milo.jpg' alt='logo' />
         </a>
-        <AiOutlineMenu className='md:hidden block
-        w-10 h-10 p-2 cursor-pointer'/>
+
+        <Favorite className="md:hidden flex" count={8} />
+
+        {showNav ? <HiMenuAlt3
+          onClick={() => setShowNav(!showNav)}
+          className='md:hidden block
+        w-10 h-10 p-2 cursor-pointer'/>  :<HiMenuAlt2
+          onClick={() => setShowNav(!showNav)}
+          className='md:hidden block
+        w-10 h-10 p-2 cursor-pointer'/>}
       </div>
-      <ul className={(showNav ? "left-0" : "-left-full") + " md:static fixed  bottom-0 top-12 md:flex md:space-x-7 items-center md:bg-transparent md:text-gray-500 bg-gray-500 bg-opacity-90 md:w-auto w-10/12 text-white md:space-y-0 space-y-5 p-2"}>
+      <ul className={(showNav ? "left-0" : "-left-full") + " md:static fixed  bottom-0 top-12 md:flex md:space-x-7 items-center md:bg-transparent md:text-gray-500 bg-gray-500 bg-opacity-90 md:w-auto w-10/12 text-white md:space-y-0 space-y-5 p-2 transition-left"}>
         <NavItem content="home" href="/home" />
         <NavItem content="Sign Up" href="/signup" />
         <NavItem content="Login" href="/signin" />
         <NavItem content="Logout" href="/logout" />
-        <Favorite className="hidden" count={8} />
+        <Favorite className="hidden md:flex" count={8} />
 
       </ul>
     </nav>
